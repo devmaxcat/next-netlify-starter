6306e22278173132f86761c1ddd3f68519234e4b
devmaxcat <devmaxcat@gmail.com>
2024-11-22 14:19:23 -0700

    Minor UI updates

--- components/Footer.js
+++ components/Footer.js
@@ -3,24 +3,26 @@ import Nav from "./Nav";
 
 export default function Footer() {
   return (
-  <footer>
-    <div className="flex-between">
-      <Link href="/accessibility">Accessibility Statement</Link>
-      <Nav />
-      <div>
-      contact@maxcat.dev
-      </div>
-      
-    </div>
-   
-     <div>
-    
+    <footer>
+      <div className="flex-between">
+        <Link href="/accessibility">Accessibility Statement</Link>
+        <Nav />
+        <div>
+          Contact <br></br>
+          <a href="">contact@maxcat.dev</a><br></br>
+          <Link href={'https://github.com/devmaxcat'}>GitHub</Link>
+        </div>
 
-     Copyright © 2024. All Rights Reserved
-     
-    </div>
-    Site developed by me<br />
+      </div>
+
+      <div>
+
+
+        Copyright © 2024. All Rights Reserved
+
+      </div>
+      Site developed by me<br />
       {/* <div className="coffee-button" dangerouslySetInnerHTML={{__html: '<a href="https://www.buymeacoffee.com/devmaxcat" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style="height: 60px !important;width: 217px !important;" ></a>'}}></div> */}
-  </footer>
+    </footer>
   )
 }
